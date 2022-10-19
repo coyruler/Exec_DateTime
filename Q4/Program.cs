e5154e3e3f8ec2b89adf622d2f162fc2ee85687f
Tony <Tony@DESKTOP-8F7ATAL>
2022-10-19 19:26:13 +0800

    加入專案檔案。

---
+++ Q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //取得本月最後一天
+            DateTime dt1 = DateTime.Today;
+            DateTime dt2 = dt1.AddMonths(1);
+            DateTime dt3 = new DateTime(dt2.Year, dt2.Month ,1 );
+            DateTime dt4 = dt3.AddDays(-1);
+
+            string LastDayInMonth = $"{dt4:yyyy/MM/dd}";
+
+
+
+            Console.WriteLine("本月最後一天為:"+LastDayInMonth);
+        }
+    }
+}
---
+++ Q7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //根據不同時間, 傳回不同問安方式
+            DateTime dt1 = DateTime.Now;
+            int time = dt1.Hour;
+            if (time < 12)
+            {
+                Console.WriteLine("早安");
+            }
+            else if (time > 18)
+            {
+                Console.WriteLine("晚安");
+            }
+            else
+            {
+                Console.WriteLine("午安");
+            }
+
+        }
+    }
+}
